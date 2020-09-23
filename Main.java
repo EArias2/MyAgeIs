9f8725b1d397e4645257e96345d1eeaca93c8122
EArias2 <eduardo.arias@patelhs.org>
2020-09-23 19:46:23 +0000

    added if and else statement

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
-class Main {
+class CheckMyAge {
   public static void main(String[] args) {
     System.out.println("Hello world!");
       int myAge = 14;
       int myBirthday= 3/06;
-      int todaysDate= 0923;
-      System.out.println("I am still" + myAge + "years old.");
+      int todaysDate= 923;
+      if (myBirthday == todaysDate)
+      
+      System.out.println("I am " + myAge + " years old.");
+       else if (myBirthday < todaysDate)
+      System.out.println("I am still " + myAge + " years old.");
   
   
   
