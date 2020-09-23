48b24e716e07365e60df275c05cfd6703fe0f68c
EArias2 <eduardo.arias@patelhs.org>
2020-09-23 18:57:43 +0000

    added age ,birthday , and todays date

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+      int myAge = 14;
+      int myBirthday= 3/06;
+      int todaysDate= 0923;
+      System.out.println("I am still" + myAge + "years old.");
+  
+  
+  
+  
   }
 }
